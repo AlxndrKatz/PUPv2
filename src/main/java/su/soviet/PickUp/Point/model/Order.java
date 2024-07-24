e8494dd381463dabcf3dbcd2bed5cb52908d4820
Alexander Katsur <alexander_katzur@mail.ru>
2024-07-24 17:38:56 +0700

    Смена generation type

--- src/main/java/su/soviet/PickUp/Point/model/Order.java
+++ src/main/java/su/soviet/PickUp/Point/model/Order.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String name;
