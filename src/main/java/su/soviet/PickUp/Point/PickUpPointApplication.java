9934b7f9e94ca8ed5548f54b417b65a23b9fa737
Alexander Katsur <alexander_katzur@mail.ru>
2024-07-30 13:58:51 +0700

    Deploy

--- src/main/java/su/soviet/PickUp/Point/PickUpPointApplication.java
+++ src/main/java/su/soviet/PickUp/Point/PickUpPointApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PickUpPointApplication {
+	//
 
 	public static void main(String[] args) {
 		SpringApplication.run(PickUpPointApplication.class, args);
